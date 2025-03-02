a1758b4504fb54a642d0a05fc140b111613aa6a3
Tomasz Bawor <bawortomasz@gmail.com>
2025-03-03 06:35:20 +1300

    feat: save app config in xdg pointed dir

--- src/config/app_config.rs
+++ src/config/app_config.rs
@@ -1,13 +1,28 @@
-#[derive(Debug)]
+use std::{error::Error, fs::File, io::Write, ops::Deref};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct AppConfiguration {
-    field: String,
+    scan_path: String,
 }
 
-pub fn generate_xgd_config() {
+pub fn generate_xgd_config(scan_path: &str) -> Result<(), Box<dyn Error>> {
     let xdg_directories = xdg::BaseDirectories::with_prefix("raggy_notes");
 
     let config_file_path = xdg_directories
         .unwrap()
         .place_config_file("config.json")
         .expect("Cannot store config file");
+
+    let mut file = File::create(config_file_path)?;
+
+    let config = AppConfiguration {
+        scan_path: scan_path.to_owned(),
+    };
+    let parsed_string = serde_json::to_string(&config)?;
+
+    file.write_all(parsed_string.into_bytes().deref());
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use config::app_config::generate_xgd_config;
 mod config;
 
 fn main() {
-    generate_xgd_config()
+    generate_xgd_config("/home/tomasz/notatki");
 }
