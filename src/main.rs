86b36317992773a622604bef8b3a101e2c605279
Tomasz Bawor <bawortomasz@gmail.com>
2025-03-06 22:34:18 +1300

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use config::app_config::AppConfiguration;
 use llama::consts;
+use log::info;
 use ollama_rs::{
     generation::{chat::request::ChatMessageRequest, completion::request::GenerationRequest},
     Ollama,
@@ -12,6 +13,9 @@ mod rag;
 
 #[tokio::main]
 async fn main() {
+    initialize_logging();
+    info!("Application starting ...")
+
     // Loading config
     //let config = AppConfiguration::load().unwrap();
 
@@ -21,17 +25,22 @@ async fn main() {
     // VectorDb connection
     //let vector_db = VectorDB::new();
     //let a = vector_db.client.list_collections().await;
+    /*
+        let llama_client = Ollama::new("http://localhost", 11434);
 
-    let llama_client = Ollama::new("http://localhost", 11434);
+        let models = llama_client.list_local_models().await;
+        println!("Local modesls: ");
+        dbg!(models);
+        let req = GenerationRequest::new(
+            consts::AI_MODEL.to_string(),
+            "Czy wiesz czemu wilk tak wyje w księżycową noc?".to_string(),
+        );
 
-    let models = llama_client.list_local_models().await;
-    println!("Local modesls: ");
-    dbg!(models);
-    let req = GenerationRequest::new(
-        consts::AI_MODEL.to_string(),
-        "Czy wiesz czemu wilk tak wyje w księżycową noc?".to_string(),
-    );
+        let response = llama_client.generate(req).await;
+        //dbg!(response);
+    */
+}
 
-    let response = llama_client.generate(req).await;
-    dbg!(response);
+fn initialize_logging() {
+    env_logger::init();
 }
